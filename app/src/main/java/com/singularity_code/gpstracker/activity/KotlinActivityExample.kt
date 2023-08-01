9d105c867168df8c8f6fbda5d0847e3387a4f384
stefanus <stefanus.ayudha@gmail.com>
2023-08-01 15:21:19 +0700

    optimize import

--- app/src/main/java/com/singularity_code/gpstracker/activity/KotlinActivityExample.kt
+++ app/src/main/java/com/singularity_code/gpstracker/activity/KotlinActivityExample.kt
@@ -9,7 +9,9 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
